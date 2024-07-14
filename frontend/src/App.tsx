2d952d9f4a22c19b771bc7bb7a2a2cef92731810
Diogo Luz <diogoseicaluz@gmail.com>
2024-07-14 01:53:14 +0100

    💄 Styiling form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar } from 'lucide-react';
+import { MapPin, Calendar, ArrowRight } from 'lucide-react';
 
 export function App() {
   return (
@@ -6,19 +6,22 @@ export function App() {
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <p className="text-zinc-300 text-lg">Convide os seus amigos e planeie a sua próxima viagem!</p>
         
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
           <div className="flex items-center gap-2 flex-1">
-            <MapPin className='size-5 text-zinc-400'/>
-            <input type="text" placeholder="Para onde vai?" className="bg-transparent text-lg placeholder-zinc-400" />
+            <MapPin className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="Para onde vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none" />
           </div>
 
           <div className="flex items-center gap-2">
-            <Calendar className='size-5 text-zinc-400'/>
-            <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400" />
+            <Calendar className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-36 outline-none" />
           </div>
 
-          <button>
+          <div className="w-px h-6 bg-zinc-800" />
+
+          <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
             Continuar
+            <ArrowRight className="size-5"/>
           </button>
         </div>
         
