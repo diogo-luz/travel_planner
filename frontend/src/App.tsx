5081e823d2e7fc1a94ceaf268b7863b94e48d2bf
Diogo Luz <diogoseicaluz@gmail.com>
2024-07-14 01:38:35 +0100

    ✨ Add homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <p className="text-zinc-300 text-lg">Convide os seus amigos e planeie a sua próxima viagem!</p>
+        
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
+          <input type="text" placeholder="Para onde vai?" className="bg-transparent text-lg placeholder-zinc-400" />
+          <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400" />
+
+          <button>Continuar</button>
+        </div>
+        
+        <p className="text-zinc-500 text-sm">
+          Ao planear a sua viagem com o plann.er está a concordar com os <br /> 
+          nossos <a className="text-zinc-300 underline" href="#">termos de utilização</a> e <a className="text-zinc-300 underline" href="#">política de privacidade</a>.
+        </p>
+      </div>
+    </div>
   )
 }
